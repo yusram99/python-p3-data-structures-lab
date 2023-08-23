31c471bed75a3ea9462943206adedb6150a07cc3
Your Name <youremail@example.com>
2023-08-23 17:47:15 +0300

    commit message

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,32 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+ return [food['name'] for food in spicy_foods]
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+ return [food for food in spicy_foods if food['heat_level'] > 5]
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+ for food in spicy_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+ for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+ return None  # Return None if no matching cuisine is found
 
 def print_spiciest_foods(spicy_foods):
-    pass
+ spiciest_foods = get_spiciest_foods(spicy_foods)
+ print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+ total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+ num_foods = len(spicy_foods)
+ return total_heat_level // num_foods if num_foods > 0 else 0  
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
